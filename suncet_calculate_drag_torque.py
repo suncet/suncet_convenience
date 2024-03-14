0ce59c5dd14fe0da2cd76f9e4f677f8ee2fdbfc6
James Mason <jmason86@gmail.com>
2024-03-14 17:04:33 -0400

    Added time dependence - solar arrays not always facing ram

--- suncet_calculate_drag_torque.py
+++ suncet_calculate_drag_torque.py
@@ -1,46 +1,61 @@
-import numpy as np
+import numpy as np 
 import astropy.units as u
 from astropy.constants import G, M_earth, R_earth
-import argparse
+import pandas as pd
+from scipy.integrate import cumtrapz
+import matplotlib.pyplot as plt
 from suncet_get_spatial_average_atmospheric_density import average_atmospheric_density
+
+
+# User input (main things that will be exposed by eventual function)
+altitude = 400 * u.km
+solar_conditions = 'max'
+
+# Constants -- really these are tuneable inputs too, but don't expect to change them much for SunCET
+coefficient_of_drag = 2.5 * u.dimensionless_unscaled # depends on shape but 2.5 is typical
+asymmetric_surface_area = 0.144 * u.m**2 # area normal to velocity vector, e.g., the solar array sailing against the wind imparting torque in one direction about the spacecraft
+#torque_lever_arm = (0.36 + (0.1 - 0.061)) * u.m # distance from centroid of the asymmetric protrusion (e.g., the solar arrays) to center of mass. 0.36 m [one panel out to get to array center] + (0.1 - 0.061) m [from +Y wall to deployed cg based on CDR Bus slide 13]
+torque_lever_arm = 0.203 * u.m # distance from centroid of the asymmetric protrusion (e.g., the solar arrays) to center of mass. Evan's idea to dual deploy by first panel normal, second panel unfolds in perpindicular direction
+torque_rod_mag_field_angle = 90 * u.deg # angle between the torque rod and the Earth's magnetic field at the location of the spacecraft. Best case scenario is 90º which will provide max torque. 
+
+# Input data
+orbit_sun_vector_to_velocity_vector_angle = pd.read_csv('/Users/masonjp2/Dropbox/suncet_dropbox/9000 Processing/data/synthetic/stk_orbit/SunCET_Sun_Direction_to_Velocity_Direction_400km_Noon_Midnight.csv')
+
+# XACT-15 specs
+adcs_momentum_storage = 0.015 * u.N *u.m * u.s # amount of angular momentum the system can store. The spec sheet says that this is for the whole system, though it's not clear if each wheel can hold this much or if each is less and it knows how to transition the momentum between them. 
+torque_rod_dipole_moment = 0.2 * u.A *u.m**2 # basically the "strength" of each torque rod
+earth_mag_field_strength_in_leo = 50e-6 * u.T # it actually varies but this is a typical value
+torque_rod_torque = (torque_rod_dipole_moment * earth_mag_field_strength_in_leo * np.sin(torque_rod_mag_field_angle.to(u.radian))).to(u.N * u.m)
+
 
 def calculate_orbital_velocity(altitude):
     altitude_m = altitude.to(u.m)
-    r = R_earth + altitude_m  # Distance from the center of the Earth
+    r = R_earth + altitude_m # Distance from center of the Earth
+
     v = np.sqrt(G * M_earth / r)
     return v
 
-def main(altitude_km, solar_conditions):
-    altitude = altitude_km * u.km
 
-    # Constants
-    coefficient_of_drag = 2.5 * u.dimensionless_unscaled
-    asymmetric_surface_area = 0.144 * u.m**2
-    torque_lever_arm = (0.36 + (0.1 - 0.061)) * u.m
-    torque_rod_mag_field_angle = 90 * u.deg
+# Calcualted intermediate quantities
+atmospheric_density = average_atmospheric_density(altitude.value, solar_conditions) * u.kg / u.m**3
+velocity = calculate_orbital_velocity(altitude)
+asymmetric_surface_area_projected_vs_time = asymmetric_surface_area * np.cos(np.radians(orbit_sun_vector_to_velocity_vector_angle['Angle (deg)'].values))
+orbit_sun_vector_to_velocity_vector_angle['Time (UTCG)'] = pd.to_datetime(orbit_sun_vector_to_velocity_vector_angle['Time (UTCG)'], format="%d %b %Y %H:%M:%S.%f")
+minutes_since_start = ((orbit_sun_vector_to_velocity_vector_angle['Time (UTCG)'] - orbit_sun_vector_to_velocity_vector_angle['Time (UTCG)'].iloc[0]).dt.total_seconds() / 60).values
 
-    # XACT-15 specs
-    adcs_momentum_storage = 0.015 * u.N * u.m * u.s
-    torque_rod_dipole_moment = 0.2 * u.A * u.m**2
-    earth_mag_field_strength_in_leo = 50e-6 * u.T
-    torque_rod_torque = (torque_rod_dipole_moment * earth_mag_field_strength_in_leo * np.sin(torque_rod_mag_field_angle.to(u.radian))).to(u.N * u.m)
+# Calculate main values of interest
+torque_drag_peak = (1/2 * atmospheric_density * velocity**2 * coefficient_of_drag * asymmetric_surface_area * torque_lever_arm).to(u.N * u.m)
+time_to_saturate_wheels_if_always_peak_drag = (adcs_momentum_storage/torque_drag_peak).to(u.minute) # From wheel at 0 speed, how long until it saturates with disturbance torques above?
+torque_drag_vs_time = (1/2 * atmospheric_density * velocity**2 * coefficient_of_drag * asymmetric_surface_area_projected_vs_time * torque_lever_arm).to(u.N * u.m)
+cumulative_torque = cumtrapz(torque_drag_vs_time, (minutes_since_start * 60), initial=0) * torque_drag_vs_time.unit * u.s
+target_index = np.argmax(cumulative_torque >= adcs_momentum_storage)
+target_time = minutes_since_start[target_index] if cumulative_torque[target_index] >= adcs_momentum_storage else None
 
-    # Calculated quantities
-    atmospheric_density = average_atmospheric_density(altitude.value, solar_conditions) * u.kg / u.m**3
-    velocity = calculate_orbital_velocity(altitude)
-    torque_drag = (1/2 * atmospheric_density * velocity**2 * coefficient_of_drag * asymmetric_surface_area * torque_lever_arm).to(u.N * u.m)
-    time_to_saturate_wheels = (adcs_momentum_storage / torque_drag).to(u.minute)
 
-    if torque_drag < torque_rod_torque:
-        print('The disturbance torque is less than what any individual torque rod can dump.')
-    else:
-        print(f'The disturbance torque is greater than what the torque rods can dump. The wheels will saturate in {time_to_saturate_wheels:.2f} (assuming best case that they started from rest).')
+if torque_drag_peak < torque_rod_torque: 
+    print('The disturbance torque is less than what any individual torque rod can dump')
+else: 
+    print('The disturbance torque is greater than what the torque rods can dump. The wheels will saturate in {:.2f} minutes (assuming best case that they started from rest).'.format(target_time))
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Calculate satellite torque and wheel saturation time.')
-    parser.add_argument('altitude', type=float, help='Altitude in kilometers')
-    parser.add_argument('solar_conditions', choices=['min', 'mean', 'max'], help='Solar conditions: min, mean, or max')
 
-    args = parser.parse_args()
-
-    main(args.altitude, args.solar_conditions)
+pass
